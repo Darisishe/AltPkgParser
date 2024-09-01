dd0fd7cc4eb5cd30d6bc4b8b43afe3dc0001ed2d
Darisishe <konev.sa@phystech.edu>
2024-09-01 04:19:14 +0300

    Structures for /export/branch_binary_packages/ response

--- src/api_struct.rs
+++ src/api_struct.rs
@@ -16,3 +16,30 @@ pub struct ArchData {
     pub arch: data::Architecture,
     count: usize,
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// describes the response of "/export/branch_binary_packages/"
+
+#[derive(Deserialize, Debug)]
+pub struct BranchPkgs {
+    pub packages: Vec<PkgDescription>,
+
+    // unused
+    requested_args: serde_json::Value,
+    length: usize,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PkgDescription {
+    name: String,
+    version: String,
+
+    // unused
+    epoch: i32,
+    release: String,
+    arch: data::Architecture,
+    disttag: String,
+    buildtime: i32,
+    source: String,
+}
+
--- src/fetch.rs
+++ src/fetch.rs
@@ -10,16 +10,16 @@ const ARCHS_URL: &str = "https://rdb.altlinux.org/api/site/all_pkgset_archs?bran
 /// # Example
 ///
 /// ```
-/// use altpkgparser::fetch::fetch_branch_archs;
-/// use altpkgparser::data::Architecture;
-/// 
-/// tokio_test::block_on(async {
-///   let res = fetch_branch_archs("p10").await.unwrap();
+/// # use altpkgparser::fetch::fetch_branch_archs;
+/// # use altpkgparser::data::Architecture;
+/// #
+/// # tokio_test::block_on(async {
+/// let res = fetch_branch_archs("p10").await.unwrap();
 ///
-///   assert!(res.contains(&Architecture("x86_64".to_owned())));
-///   assert!(res.contains(&Architecture("aarch64".to_owned())));
-///   assert!(res.contains(&Architecture("noarch".to_owned())));
-/// });
+/// assert!(res.contains(&Architecture("x86_64".to_owned())));
+/// assert!(res.contains(&Architecture("aarch64".to_owned())));
+/// assert!(res.contains(&Architecture("noarch".to_owned())));
+/// # });
 /// ```
 pub async fn fetch_branch_archs(branch_name: &str) -> Result<Vec<Architecture>> {
     let branch_archs: api_struct::PkgsetArchs = reqwest::get(ARCHS_URL.to_owned() + branch_name)
